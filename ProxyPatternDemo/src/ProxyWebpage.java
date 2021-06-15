83aa3340cdd12db196a5d48609ab92b6f48ecfad
Tylor Schlosser <56730822+DarkTerrorWolf@users.noreply.github.com>
2021-06-15 14:18:35 -0700

    Added protect proxy example

--- ProxyPatternDemo/src/ProxyWebpage.java
+++ ProxyPatternDemo/src/ProxyWebpage.java
@@ -4,15 +4,22 @@ import java.util.Set;
 public class ProxyWebpage implements IProxyInterfrace {
     private String url;
     private RealWebpage webpage;
-
+    private Set<String> blocksites = new HashSet<>();
 
     public ProxyWebpage(String url) {
         this.url = url;
+        blocksites.add("abc.com");
+        blocksites.add("schoolstuff.com");
+        blocksites.add("egator.greenriver.edu");
     }
 
     @Override
     public void display() {
-        if(webpage==null){
+        if(blocksites.contains(url)){
+            System.out.println("Access denied! "+url+" has been blocked by the admin.");
+            return;
+        }
+        else if(webpage==null){
             webpage = new RealWebpage(url);
         }
         webpage.display();
